59932476139287f44090f010f6622e6f7fa82f95
Sarthak Pradhan <pradhansarthak2016@gmail.com>
2025-01-01 21:14:20 +1100

    transaction form added

--- mon/src/pages/home/Home.js
+++ mon/src/pages/home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import styles from './Home.module.css'
+import TransactionForm from './TransactionForm'
 
 
 export default function home() {
   return (
-    <div>homeie</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        Transaction list
+        
+      </div>
+
+      <div className={styles.sidebar}>
+        
+        <TransactionForm/>
+      </div>
+
+    </div>
   )
 }
